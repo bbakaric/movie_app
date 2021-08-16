047ef70afb5e91cbca23f68beb13151d6acef671
Bruno Bakarić <bak.13@hotmail.com>
2021-08-16 12:47:50 +0200

    fast edit of few lines and displayed release year

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
-import { getMovies, getPoster } from '../store/action-creators';
+import { getMovies } from '../store/action-creators';
 
 const MovieCard = () => {
   const posterUrl: string = 'https://image.tmdb.org/t/p/w500';
@@ -14,11 +14,12 @@ const MovieCard = () => {
 
   return (
     <div>
+      <h1>Movies</h1>
       {movies.map((movie) => (
         <div key={movie.id}>
           <h1>{movie.original_title}</h1>
           <img src={posterUrl + movie.poster_path} alt="poster" />
-          <p>Release: {movie.release_date}</p>
+          <p>Release: {parseInt(movie.release_date)}</p>
           <h3>Rating: {movie.vote_average}</h3>
         </div>
       ))}
